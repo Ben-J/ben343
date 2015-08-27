5e87bafefed6ac59ec80f93e91975241728d930d
Ben <Ben@mobyac.home>
2015-08-28 00:37:42 +0200

    Amélioration fonctionnalité fifaCup

--- ben343/src/main/resources/static/js/controllers/fifaControllers.js
+++ ben343/src/main/resources/static/js/controllers/fifaControllers.js
@@ -5,6 +5,10 @@ var benJControllers = angular.module('FifaControllers', []);
 benJControllers.controller('FifaCtrl', ['$scope', '$log',
 	function($scope, $log) {
 		$scope.players = [];
+		$scope.scores = [];
+		
+		$scope.playersCandidates1 = [];
+		$scope.playersCandidates2 = [];
 		
 		$scope.playerPlaceholder = "Choisir un joueur";
 		
@@ -19,16 +23,18 @@ benJControllers.controller('FifaCtrl', ['$scope', '$log',
 			if(exist === false) {
 				var nP = 
 					{
-						nom:$scope.newPlayer, 
-						gagne:0, 
-						perdu:0, 
-						nul:0,
+						nom: $scope.newPlayer, 
+						gagne: 0, 
+						perdu: 0, 
+						nul: 0,
 						points: 0, 
 						butsMarques: 0,
 						butsEncaisses: 0, 
 						butsMoyenne: 0
 					};
 				$scope.players.push(nP);
+				$scope.playersCandidates1.push(nP);
+				$scope.playersCandidates2.push(nP);
 				$scope.newPlayer = "";				
 			}
 		};
@@ -62,6 +68,36 @@ benJControllers.controller('FifaCtrl', ['$scope', '$log',
 					} else { row.nul++; }
 				}
 			});
+			$scope.scores.push($scope.newScore);
+			$scope.newScore = {j1:{}, j2:{}, butJ1:0, butJ2:0};
 		};
+		
+		$scope.getCurrentPlayers = function() {
+			$scope.currentPlayers = [];
+			if (angular.isDefined($scope.newScore.j1) || angular.isDefined($scope.newScore.j2)) {
+				angular.forEach($scope.players, function(row, index){
+					if(angular.isDefined($scope.newScore.j1) && !angular.equals(row.nom, $scope.newScore.j1.nom) && 
+					   angular.isDefined($scope.newScore.j2) && !angular.equals(row.nom, $scope.newScore.j2.nom)) {
+						$scope.currentPlayers.push(row);
+					}
+				});
+			}
+			return $scope.currentPlayers;
+		};
+		
+		$scope.$watch("newScore.j1", function(newVal, oldVal) {
+			if(angular.isDefined(newVal)) {
+				if(angular.isDefined(oldVal)) {
+					$scope.playersCandidates2.push(oldVal);
+				}
+				angular.forEach($scope.playersCandidates2, function(row, index){
+					if(angular.equals(newVal.nom, row.nom)) {
+						$scope.playersCandidates2.splice(index, 1);
+					}
+				});
+			} else {
+				$scope.playersCandidates2.push(oldVal);
+			}
+		});
   	}
 ]);
