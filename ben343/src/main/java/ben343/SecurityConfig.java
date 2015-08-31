34ef4dc0746f2be65795cc5ad6b117530841de8c
Ben <Ben@mobyac.home>
2015-08-31 23:43:50 +0200

    Access a FifaCup par mot de passe.

--- ben343/src/main/java/ben343/SecurityConfig.java
+++ ben343/src/main/java/ben343/SecurityConfig.java
@@ -1,12 +1,15 @@
 package ben343;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -23,15 +26,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**");
 	}
-
+	
+	@Bean
+	public AuthenticationSuccessHandler successHandler() {
+	    SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+	    handler.setUseReferer(true);
+	    return handler;
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable(); // TODO FIXME NOT SAFE
-//				authorizeRequests().antMatchers("/*").permitAll().;
-//				.antMatchers("/admin/**").hasRole("ADMIN")
-//				.anyRequest().authenticated()
-//				.and().formLogin()
-//				.loginUrl("/login")
-//				.permitAll();
+		http.authorizeRequests()
+		.antMatchers("/views/home/home.html").permitAll()
+		.antMatchers("/views/home/parts/skills/skills.html").permitAll()
+        .antMatchers("/views/resume/resume.html").permitAll()
+        .antMatchers("/views/resume/carousel.html").permitAll()
+        .antMatchers("/views/cv/expPro.html").permitAll()
+        .antMatchers("/views/privacy/privacy.html").permitAll()
+        .antMatchers("/views/home/parts/skills/skills.html").permitAll()
+        .antMatchers("/views/home/parts/projects/ups-projects.html").permitAll()
+        .antMatchers("/views/home/home.html").permitAll()
+        .antMatchers("/views/error.html").permitAll()
+        .antMatchers("/login.html").permitAll()
+        .antMatchers("/views/fifa/fifaCup.html").authenticated()
+        .and().csrf().disable() // Disable CSRF
+	    .formLogin()
+//	        .loginPage("/login")
+	        .permitAll()
+	        .successHandler(successHandler())
+	        .and()
+	    .logout()
+	        .permitAll();
 	}
 }
--- ben343/src/main/java/ben343/controllers/IndexController.java
+++ ben343/src/main/java/ben343/controllers/IndexController.java
@@ -1,8 +1,12 @@
 package ben343.controllers;
 
+import java.security.Principal;
+
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController implements ErrorController {
@@ -17,5 +21,21 @@ public class IndexController implements ErrorController {
 	@Override
 	public String getErrorPath() {
 		return PATH;
+	}
+	
+	// for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied(Principal user) {
+
+		ModelAndView model = new ModelAndView();
+
+		if (user != null) {
+			model.addObject("msg", "Hi " + user.getName() + ", you do not have permission to access this page!");
+		} else {
+			model.addObject("msg", "You do not have permission to access this page!");
+		}
+
+		model.setViewName("403");
+		return model;
 	}
 }
